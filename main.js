f487e7540daa5ad39d9bab4b53ad44f9eb24fc8d
Hien Nguyen <hienqnguyen95@berkeley.edu>
2020-06-29 22:14:13 -0700

    add select text

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-$(window).on("load", function () {
-    // Handler when all assets (including images) are loaded
-    alert("This is working");
-    console.log("dwadaw")
-});
+// $(window).on("load", function () {
+//     // Handler when all assets (including images) are loaded
+//     alert("This is working");
+//     console.log("dwadaw")
+// });
 
+console.log("alll");
+
+window.addEventListener('mouseup', wordSelected);
+
+function wordSelected(){
+    let selectedText = window.getSelection().toString();
+    console.log(selectedText);
+}
