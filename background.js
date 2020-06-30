d63a039a7d6a85b69e53b0d296a7298f35cb2bf3
Hien Nguyen <hienqnguyen95@berkeley.edu>
2020-06-30 01:18:26 -0700

    finish, editting the box to make it look nice

--- background.js
+++ background.js
@@ -2,6 +2,9 @@ console.log('background running')
 
 chrome.runtime.onMessage.addListener(receiver);
 
+window.word = "coding train";
+
 function receiver(request, sender, sendresp){
-    
+    console.log(request);
+    window.word = request.text;
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,33 @@
-function setup(){
-    noCanvas();
+// Wordnik API key:
+// let api_key = '/?api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7'
+
+function setup() {
     let bgpage = chrome.extension.getBackgroundPage();
-    let word = bgpage.word;
-    createP(word);
+    let word = bgpage.word.trim();
+    src = `https://books.google.com/ngrams/interactive_chart?content=${word}&year_start=1800&year_end=2008&corpus=15&smoothing=3&share=&direct_url=t1%3B%2C${word}%3B%2Cc0`;
+    // frame = document.getElementsByTagName('iframe');
+    // frame.src = src;
+    const node = document.createElement("iframe");
+    node.src = src
+    node.width = 900;
+    node.height = 500;
+    node.marginwidth = 0;
+    node.marginheight = 0;
+    node.hspace = 0;
+    node.vspace = 0;
+    node.frameborder = 0;
+    node.scrolling = "no";
+    //     < iframe name = "ngram_chart"
+    // src = ""
+    // width = 900 height = 500 marginwidth = 0 marginheight = 0 hspace = 0 vspace = 0 frameborder = 0 scrolling = no ></iframe >
+    const body = document.getElementsByTagName("BODY")[0];
+    body.appendChild(node);
+    // loadJSON(url, gotData);
+    // function gotData(data) {
+    //     createP(data[0].text).style('font-size', '48pt');
+    // }
+    //createP(word);
 }
+
+setup();
+
